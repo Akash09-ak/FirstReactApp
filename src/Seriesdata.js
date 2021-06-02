6cbf0a96144876d6d0d36ee7c689377d4ef78148
Akash09-ak <akashkuk@gmail.com>
2021-06-03 01:07:10 +0530

    first commit

---
+++ src/Seriesdata.js
@@ -0,0 +1,82 @@
+const Seriesdata=[
+    {  
+        imgsrc:"https://wallpapersflix.com/wp-content/uploads/2020/07/Dark-Netflix-Wallpaper-HD.jpg",
+        title:"Netflix Original Series",
+        sname:"Dark",
+        link:"https://www.netflix.com/in/title/80100172"   
+    },
+    {
+        imgsrc:"https://www.newasianpost.com/wp-content/uploads/2018/05/Sacred-Games.jpg",
+        title:"Netflix Original Series",
+        sname:"Sacred Games",
+        link:"https://www.netflix.com/in/title/80115328"   
+    },
+    {
+        imgsrc:"https://2.bp.blogspot.com/-cjsR78xb9xA/W1eOC-KRJNI/AAAAAAAABJ4/ZvczsjKQzlMx3xwk6isZiisx-RW0EIYNgCLcBGAs/s1600/13-Reasons-Why-Poster-.jpg",
+        title:"Netflix Original Series",
+        sname:"13 Reasons Why",
+        link:"https://www.netflix.com/in/title/80117470"   
+
+   },
+   {
+    imgsrc:"https://light-in-the-attic.s3.amazonaws.com/uploads/release_image/21360/image/tmp_2F1510609752740-chminsdrqzd-41dced5fb079766231ddabab09cb4026_2F780163510126.jpg",
+    title:"Netflix Original Series",
+    sname:"Stranger Things",
+    link:"https://www.netflix.com/in/title/80057281"   
+},
+{
+    imgsrc:"https://vignette.wikia.nocookie.net/netflix/images/1/14/You_Netflix_Promotional.jpg/revision/latest?cb=20191219171623",
+    title:"Netflix Original Series",
+    sname:"You",
+    link:"https://www.netflix.com/in/title/80211991"   
+
+},
+{
+    imgsrc:"https://tvguide1.cbsistatic.com/rovi/showcards/tvshow/297527/thumbs/16917305_900x1200.jpg",
+    title:"Netflix Original Series",
+    sname:"The Vampire Dairies",
+    link:"https://www.netflix.com/in/title/70143860"   
+},
+{
+    imgsrc:"https://getwallpapers.com/wallpaper/full/0/b/9/1099082-peaky-blinders-wallpapers-1920x1080-for-4k.jpg",
+    title:"Netflix Original Series",
+    sname:"Peaky Blinders",
+    link:"https://www.netflix.com/in/title/80002479"   
+
+},
+{
+    imgsrc:"https://news.otakukart.com/wp-content/uploads/2019/01/Mirzapur.jpg",
+    title:"Amazon Prime Original Series",
+    sname:"Mirzapur",
+    link:"https://www.primevideo.com/detail/Mirzapur/0PDOKMV9CRLOMO5EUKNCUJLG4Q"   
+},
+{
+    imgsrc:"https://cdn.headlinesoftoday.com/wp-content/uploads/2020/08/2d-1200x945-1-e1597750889477-1200x945.jpg",
+    title:"Netflix Original Series",
+    sname:"Riverdale",
+    link:"https://www.netflix.com/in/title/80133311"   
+},
+{
+    imgsrc:"https://img.reelgood.com/content/show/a1690196-5da4-447b-85c8-bf1e799dd465/poster-780.jpg",
+    title:"Netflix Original Series",
+    sname:"Money Heist",
+    link:"https://www.netflix.com/in/title/80192098"   
+
+},
+{
+    imgsrc:"https://www.assignmentx.com/wp-content/uploads/2019/09/THE-FAMILY-MAN-key-art.jpg",
+    title:"Amazon Prime Series",
+    sname:"The Family Man",
+    link:"https://www.primevideo.com/detail/The-Family-Man/0S3QYI59BAEI5KVLHCKSR91YGD"   
+
+},
+{
+    imgsrc:"https://tvguide1.cbsistatic.com/feed/1/289/thumbs/115885289_1300x1733.jpg",
+    title:"Netflix Original Series",
+    sname:"Lucifer",
+    link:"https://www.netflix.com/in/title/80057918"   
+
+}
+]
+
+export default Seriesdata;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDom from "react-dom";
+import Card from "./Card";
+import "./index.css"
+import Seriesdata from "./Seriesdata";
+
+function mycard(val)
+{
+    return(
+     <Card 
+        imgsrc={val.imgsrc}
+        title={val.title}
+        sname={val.sname}
+        link={val.link}  
+        />
+    );
+}
+
+ReactDom.render(
+    <>
+    <h1 className="heading_style">Favourites webSeries </h1>
+     {Seriesdata.map(mycard)}
+    </>,
+     document.getElementById("root")
+);
